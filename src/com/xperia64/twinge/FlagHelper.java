72f4c60b8ef60c7a7855a8de0753537967288f41
Michael Hrcek <hrcekmj@clarkson.edu>
2015-10-19 16:09:08 -0400

    Added object to list flags

---
+++ src/com/xperia64/twinge/FlagHelper.java
@@ -0,0 +1,39 @@
+package com.xperia64.twinge;
+
+/**
+ * 
+ * @author Michael Hrcek <hrcekmj@clarkson.edu>
+ * 
+ */
+public class FlagHelper {
+
+	Flag[] flags = {new Flag("-h",0,"Shows this help text."),
+			new Flag("-u",1,"Defines the url to grab the video playlist from. Use: \"--url something.something/location\"")};
+	
+	public String listFlags(){
+		String str = "";
+		
+		for(Flag f: flags){
+			str += f + "\n\n";
+		}
+		
+		return str;
+	}
+	
+	private class Flag {
+		String flag;
+		int expectedArgs;
+		String description;
+
+		public String toString(){
+			return flag + "\n" + description;
+		}
+		
+		private Flag(String flag, int expectedArgs, String description) {
+			this.flag = flag;
+			this.expectedArgs = expectedArgs;
+			this.description = description;
+		}
+	}
+
+}
